23874a1df7a72903926cdea109c5e672fd98be3e
wilbrone <wilbroneokoth@gmail.com>
2019-12-13 00:17:47 +0300

    --- Auto-Git Commit ---

--- photos/urls.py
+++ photos/urls.py
@@ -3,3 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
+
+urlpatterns=[
+    url('^$',views.news_of_day,name='newsToday'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^article/(\d+)',views.article,name ='article')
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
