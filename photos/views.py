025ad41a04f7e9a9916fc45a8a34c3a33db6429d
wilbrone <wilbroneokoth@gmail.com>
2019-12-13 11:28:10 +0300

    --- Auto-Git Commit ---

--- photos/views.py
+++ photos/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
-from django.shortcuts import render,redirect
 
 from .models import Image,Category,Location
 
+# Create your views here.
 
 def all_photos(request):
     gallery = Image.get_images()
 
     return render(request, 'all-pics/pics.html', {"gallery": gallery})
+
+def single_image(request, image_id):
+    try:
+        gallery = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+
+    return render(request, 'all-pics/img.html', {"gallery":gallery})
